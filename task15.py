6d507fd16001adf093e81795967e8abcbb5055fe
Aitmambetov <m.aytmambetov@mail.ru>
2020-02-17 00:04:35 +0600

    2.15

---
+++ task15.py
@@ -0,0 +1,10 @@
+"""
+Write the code, which will print numbers from 0 till your age. And if your age
+is odd, will be printed all odd numbers till your age, if even all even numbers.
+"""
+
+age = 33
+odd = 1
+while odd <= age:
+   print(odd)
+   odd += 2
